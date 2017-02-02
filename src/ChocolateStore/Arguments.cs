744662e257214d9ed8bece457cfe368dd1b9a981
fekir <federico.kircheis@gmail.com>
2017-02-02 18:05:10 +0100

    Initial support for new parameter (cache dir)

--- src/ChocolateStore/Arguments.cs
+++ src/ChocolateStore/Arguments.cs
@@ -2,7 +2,11 @@
 {
     class Arguments
     {
+		// where the packages are saved and referenced
         public string Directory { get; set; }
+		//where to download the package from
         public string Url { get; set; }
+		// where to save the packages
+		public string CacheDir { get; set; }
     }
 }
